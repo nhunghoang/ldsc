dcc75b40f6874bdbc219fedf5feae349b9cdd852
James Baker <james.baker@vanderbilt.edu>
2024-04-10 08:54:28 -0500

    bumped the patch number on the version to represent a bug fix

--- ldsc.py
+++ ldsc.py
@@ -27,7 +27,7 @@ try:
 except AttributeError:
     raise ImportError('LDSC requires pandas version >= 0.17.0')
 
-__version__ = '2.0.0'
+__version__ = '2.0.2'
 MASTHEAD = "*********************************************************************\n"
 MASTHEAD += "* LD Score Regression (LDSC)\n"
 MASTHEAD += "* Version {V}\n".format(V=__version__)
